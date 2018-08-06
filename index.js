b94f89043cb3a44abdd45f275d67fd8bd5a51a19
Rose <rose1264@gmail.com>
2018-08-06 14:09:05 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+const produceDrivingRange = function(blockRange){
+  return function(start, end){
+    let s = parseInt(start);
+    let e = parseInt(end);
+    let distance = Math.abs(s - e);
+    let diff = blockRange - distance;
+
+    if (diff > 0){
+      return `within range by ${diff}`
+    } else {
+      return `${Math.abs(diff)} blocks out of range`
+    }
+  }
+}
+
+const produceTipCalculator = function(percentage){
+  return function(rideFare){
+    return rideFare * percentage
+  }
+}
