a2d0f06bfe624228fbd4e72160dd7641e171e7c1
Rose <rose1264@gmail.com>
2018-08-07 13:25:11 -0400

    Done.

--- index.js
+++ index.js
@@ -18,3 +18,13 @@ const produceTipCalculator = function(percentage){
     return rideFare * percentage
   }
 }
+
+function createDriver(){
+  let driverId = 0
+  return class {
+    constructor(name){
+      this.id = ++driverId
+      this.name = name
+    }
+  }
+}
